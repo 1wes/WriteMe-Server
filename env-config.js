8411ff5af8a51da739b1489de1223b41b2a1e622
1wes <okemwawes@gmail.com>
2023-08-14 16:40:41 +0300

    export database connection creation object details

--- env-config.js
+++ env-config.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
-const {PORT}=process.env;
+const {PORT, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME}=process.env;
 
 module.exports={
-    port:PORT
+    port:PORT,
+    db_user:DB_USERNAME,
+    db_password:DB_PASSWORD,
+    db_host:DB_HOST,
+    db_name:DB_NAME
 }
