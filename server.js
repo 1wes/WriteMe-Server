ac260d51a22451e0db57483f26225399f267c4a0
1wes <okemwawes@gmail.com>
2023-08-14 17:48:43 +0300

    mount route handlers

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const app=express();
 const {port}=require('./env-config');
-const router=express.Router();
 const cors=require('cors');
+const ordersAPI=require('./routes/orders');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({}));
+
+app.use("/api", ordersAPI);
 
 app.listen(port, ()=>{
     console.log(`Server is running at port ${port}`);
