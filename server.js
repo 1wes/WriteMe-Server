6a7f7c06ce672a26843d1fa3dc3c63cda250e6e0
1wes <okemwawes@gmail.com>
2023-08-14 16:26:32 +0300

    initialize server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express=require('express');
 const app=express();
-const port=require('./env-config');
+const {port}=require('./env-config');
+const router=express.Router();
+const cors=require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors({}));
 
 app.listen(port, ()=>{
     console.log(`Server is running at port ${port}`);
