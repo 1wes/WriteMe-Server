8a839d1454271eca825b624ee8377b494a8107d3
1wes <okemwawes@gmail.com>
2023-08-15 19:23:16 +0300

    mount endpoint paths

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app=express();
 const {port}=require('./env-config');
 const cors=require('cors');
 const ordersAPI=require('./routes/orders');
+const clients=require("./routes/users");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({}));
 
 app.use("/api", ordersAPI);
+app.use("/api/user", clients);
 
 app.listen(port, ()=>{
     console.log(`Server is running at port ${port}`);
