599e9fcaf61cdf91d02b6c42aeb949ce53f3a72d
1wes <okemwawes@gmail.com>
2023-08-15 19:24:32 +0300

    join static files path absolutely

--- routes/orders.js
+++ routes/orders.js
@@ -1,12 +1,17 @@
 const express=require('express');
 const router=express.Router();
 const dbConnection=require('../db');
+const path=require('path');
+const app=express();
+const fs=require('fs');
+
+app.use(express.static(path.join(__dirname, "public")));
 
 router.use((req, res, next)=>{
     next();
 })
 
-router.get("/", (req, res)=>{
+router.get("/", (req, res)=>{ 
 
 });
 
