d3bf73bb95a7c8a9fe2c2862feed5524bb1acb94
1wes <okemwawes@gmail.com>
2023-08-18 13:10:22 +0300

    hash password and persist user info in storage

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,44 @@
 const express=require('express');
 const router=express.Router();
+const dbConnection=require('../db');
+const hashPasssword=require('../utils/password');
+const generateUserId=require('../utils/userId');
 
 router.use((req, res, next)=>{
     next();
 });
 
-router.get("/register", (req, res)=>{
-    res.send("am the register page")
+router.post("/register", async(req, res)=>{
+    
+    const {firstName, lastName, email, dialCode, phoneNumber, confirmPassword}=req.body;
+
+    let userId=generateUserId();
+
+    const phone=dialCode+phoneNumber;
+
+    const hashedPassword=await hashPasssword(confirmPassword);
+
+    let userInfo=[
+        userId, firstName, lastName, email, phone, hashedPassword
+    ]
+
+    let sqlStatement=`INSERT INTO users (uuid, first_name, last_name, email, phone_no, password) VALUES (?)`;
+
+    dbConnection.query(sqlStatement, [userInfo], (err, result)=>{
+
+        if(err){
+            console.log(err)
+        }
+
+        console.log(result);
+    })
+
+    dbConnection.end();
+
 });
 
-router.get("/login", (req, res)=>{
+router.post("/login", (req, res)=>{
 
-    res.send("Am the login page");
 });
 
 module.exports=router;
