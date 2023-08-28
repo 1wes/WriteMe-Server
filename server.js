e61783faa7bdc6ebb9529cb01faabb2851480750
1wes <okemwawes@gmail.com>
2023-08-29 00:04:40 +0300

    set origin from which the browser can load resources

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const {port}=require('./env-config');
+const {port, client_origin}=require('./env-config');
 const cors=require('cors');
 const ordersAPI=require('./routes/orders');
 const clients=require("./routes/users");
@@ -8,7 +8,7 @@ const clients=require("./routes/users");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:client_origin,
     credentials:true
 }));
 
