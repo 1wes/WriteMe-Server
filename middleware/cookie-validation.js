3d1c1dbf80188eeaac689368f7b61506ac06f333
1wes <okemwawes@gmail.com>
2023-09-01 22:13:19 +0300

    verify token stored in request cookie

---
+++ middleware/cookie-validation.js
@@ -0,0 +1,54 @@
+const jwt=require('jsonwebtoken');
+const {secret_key}=require('../env-config');
+
+let verifyToken=(req, res, next)=>{
+
+    let authCookie=req.cookies.authorizationToken;
+
+    if(authCookie){
+
+        const isTokenValid=()=>{
+
+            return tokens=jwt.verify(authCookie, secret_key, (err, decoded)=>{
+
+                if(err){
+
+                    errorObject={
+                        statusCode:401, 
+                        message:err.message
+                    }
+
+                    res.clearCookie("authorizationToken", {domain:'localhost', path:'/'});
+
+                    return new Error(errorObject);
+                }
+
+                return decoded;
+            })
+        }
+
+        const tokenIsValid=isTokenValid();
+
+        if(tokenIsValid){
+
+            statusCode=200;
+
+            tokenInfo=tokenIsValid;
+
+            next();
+        }else{
+
+            statusCode=errorInfo.statusCode;
+
+            errorMessage=errorInfo.message;
+
+            next();
+        }
+    }else{
+
+        statusCode=403;
+
+        next();
+    }
+}
+module.exports=verifyToken;
