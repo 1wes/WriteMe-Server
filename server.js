d8ea0279257c5406dfa32b16ab69f203a22d94d9
1wes <okemwawes@gmail.com>
2023-09-01 21:50:53 +0300

    mount cookie parser class

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const {port, client_origin}=require('./env-config');
 const cors=require('cors');
 const ordersAPI=require('./routes/orders');
 const clients=require("./routes/users");
+const cookieParser=require('cookie-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -11,6 +12,7 @@ app.use(cors({
     origin:client_origin,
     credentials:true
 }));
+app.use(cookieParser());
 
 app.use("/api", ordersAPI);
 app.use("/api/user", clients);
