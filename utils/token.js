c6cbd8f9575400ac17fb231c58bbb20811425a70
1wes <okemwawes@gmail.com>
2023-09-05 18:39:37 +0300

    add more payload properties

--- utils/token.js
+++ utils/token.js
@@ -1,9 +1,9 @@
 const jwt=require('jsonwebtoken');
 const {secret_key}=require('../env-config');
 
-const generateToken=(email)=>{
+const generateToken=(email, uuid)=>{
 
-    return jwt.sign({email:email}, secret_key, {expiresIn:"1h"})
+    return jwt.sign({email:email, uuid:uuid}, secret_key, {expiresIn:"1h"})
 }
 
 module.exports=generateToken;
