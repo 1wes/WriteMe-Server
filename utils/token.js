027f44c7ac68a5aef43445dabe0f4c432c750535
1wes <okemwawes@gmail.com>
2023-09-18 14:26:51 +0300

    pass role argument to token generation function

--- utils/token.js
+++ utils/token.js
@@ -1,7 +1,7 @@
 const jwt=require('jsonwebtoken');
 const {secret_key}=require('../env-config');
 
-const generateToken=(email, uuid)=>{
+const generateToken=(email, uuid, role)=>{
 
     return jwt.sign({email:email, uuid:uuid, role:role}, secret_key, {expiresIn:"1h"})
 }
