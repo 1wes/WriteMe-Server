89fdfe24cb566396018982e365615966289ea3c5
1wes <okemwawes@gmail.com>
2023-09-19 20:45:21 +0300

    add user name to token payload

--- utils/token.js
+++ utils/token.js
@@ -1,9 +1,9 @@
 const jwt=require('jsonwebtoken');
 const {secret_key}=require('../env-config');
 
-const generateToken=(email, uuid, role)=>{
+const generateToken=(email, uuid, role, firstName, lastName)=>{
 
-    return jwt.sign({email:email, uuid:uuid, role:role}, secret_key, {expiresIn:"1h"})
+    return jwt.sign({firstName:firstName, lastName:lastName,email:email, uuid:uuid, role:role}, secret_key, {expiresIn:"1h"})
 }
 
 module.exports=generateToken;
