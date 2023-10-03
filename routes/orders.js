2b2e780bf6c287e71bc768e2e22157ebfa09f49f
1wes <okemwawes@gmail.com>
2023-10-03 20:24:33 +0300

    sort orders by latest added

--- routes/orders.js
+++ routes/orders.js
@@ -27,7 +27,7 @@ router.get("/admin", verifyToken, (req, res)=>{
             if(tokenInfo.role==="Admin"){
                 const username=`${tokenInfo.firstName} ${tokenInfo.lastName}`
             
-                const allOrders=`SELECT orders.*, first_name, last_name FROM orders INNER JOIN users ON orders.created_by=uuid`;
+                const allOrders=`SELECT orders.*, first_name, last_name FROM orders INNER JOIN users ON orders.created_by=uuid ORDER by id DESC`;
                 
                 dbConnection.query(allOrders, (err, result)=>{
     
