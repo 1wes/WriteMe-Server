332b5e81caa3862c3d64af92be95fc54d7cf7d3f
1wes <okemwawes@gmail.com>
2023-11-01 15:49:50 +0300

    minor change

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
-const mysql=require('mysql');
-const {db_name, db_password, db_host, db_user}=require('./env-config');
+const mysql = require('mysql');
+const { db_name, db_password, db_host, db_user } = require('./env-config');
 
-const dbConnection=mysql.createConnection({
-    host:db_host,
-    user:db_user,
-    password:db_password,
-    database:db_name
+const dbConnection = mysql.createConnection({ 
+    host: db_host,
+    user: db_user,
+    password: db_password,
+    database: db_name
 });
 
-dbConnection.connect(err=>{
+dbConnection.connect(err => {
 
-    if(err){
+    if (err) {
         throw err
     }
 
@@ -18,4 +18,4 @@ dbConnection.connect(err=>{
 
 });
 
-module.exports=dbConnection;
+module.exports = dbConnection;
