1d2809084ff6d0d54f17bd5b1bb1887e39620b60
1wes <okemwawes@gmail.com>
2024-08-06 17:43:24 +0300

    chore: move js files to src directory

--- utils/token.js
+++
@@ -1,9 +0,0 @@
-const jwt=require('jsonwebtoken');
-const {secret_key}=require('../env-config');
-
-const generateToken=(email, uuid, role, firstName, lastName)=>{
-
-    return jwt.sign({firstName:firstName, lastName:lastName,email:email, uuid:uuid, role:role}, secret_key, {expiresIn:"1h"})
-}
-
-module.exports=generateToken;
