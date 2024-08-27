9f12e4560e02f5a5463fb634123c6693c95af09b
1wes <okemwawes@gmail.com>
2024-08-27 19:30:59 +0300

    feat(password): pass salt as a parameter to the password hashing function

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,11 +1,11 @@
 import bcrypt from 'bcrypt'
 
-export const hashPasssword=async(plain_password:string)=>{
-
-    return await bcrypt.hash(plain_password, 10) ;
+export const hashPasssword = async (plain_password: string, salt: string): Promise<string> => {
+    
+    return await bcrypt.hash(plain_password, salt);
 }
 
-export const comparePassword=async (plain_password:string, hashed_password:string)=>{
+export const comparePassword=async (plain_password:string, hashed_password:string):Promise<boolean>=>{
 
     return await bcrypt.compare(plain_password, hashed_password);
 }
