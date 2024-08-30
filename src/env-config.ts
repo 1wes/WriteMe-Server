922588d3022698dc75da46c16777ca75975f7e74
1wes <okemwawes@gmail.com>
2024-08-30 17:24:24 +0300

    feat(env-config): update config objects to reflect removed db variables

--- src/env-config.ts
+++ src/env-config.ts
@@ -5,10 +5,6 @@ dotenv.config();
 
 const {
   PORT,
-  DB_USERNAME,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_NAME,
   TOKEN_SECRET_KEY,
   CLIENT_ORIGIN_URL,
   EMAIL,
@@ -18,10 +14,6 @@ const {
 // check whether all env variables are loaded
 if (
   !PORT ||
-  !DB_USERNAME ||
-  !DB_PASSWORD ||
-  !DB_HOST ||
-  !DB_NAME ||
   !TOKEN_SECRET_KEY ||
   !CLIENT_ORIGIN_URL ||
   !EMAIL ||
@@ -32,10 +24,6 @@ if (
 
 const config: EnvConfig = {
   port: PORT,
-  db_user: DB_USERNAME,
-  db_password: DB_PASSWORD,
-  db_host: DB_HOST,
-  db_name: DB_NAME,
   secret_key: TOKEN_SECRET_KEY,
   client_origin: CLIENT_ORIGIN_URL,
   senderEmail: EMAIL,
