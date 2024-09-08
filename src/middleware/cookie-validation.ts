07b9d37daf31e843626b9037af05faff3bbd309e
1wes <okemwawes@gmail.com>
2024-09-08 19:12:57 +0300

    feat(cookie): make verifyToken a middleware

--- src/middleware/cookie-validation.ts
+++ src/middleware/cookie-validation.ts
@@ -9,48 +9,23 @@ let verifyToken=(req:Request, res:Response, next:NextFunction):any=>{
 
     let authCookie: string | undefined = req.cookies.authorizationToken;
 
-    if(authCookie){
+    if (!authCookie) {
+        return res.sendStatus(403);
+    }
 
-        const isTokenValid=()=>{
+    try {
+        
+        const decoded = jwt.verify(authCookie as string, secret_key as string | Buffer);
 
-            return jwt.verify(authCookie as string, secret_key as string | Buffer, (err, decoded)=>{
+        req.statusCode = 200;
 
-                if(err){
-
-                    return {
-                        code:401,
-                        message:err.message
-                    }
-
-                }else{
-                    return {
-                        code:200,
-                        message:decoded
-                    }
-                }
-            })
-        }
-
-        const tokenStatus: any = isTokenValid();
-
-        if(tokenStatus.code===200){
-
-            req.statusCode=tokenStatus.code;
-
-            req.tokenInfo=tokenStatus.message
-                            
-            next();
-        }else{
-
-            req.statusCode=tokenStatus.code;
-
-            next();
-        }
-    }else{
-
-        req.statusCode=403;
+        req.tokenInfo = decoded;
 
         next();
+
+    } catch (err) {
+        
+        return res.sendStatus(401);
     }
 }
 export default verifyToken;
