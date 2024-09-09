06fe1fe6921de604b17a584ea91c0cde4f9c76fd
1wes <okemwawes@gmail.com>
2024-09-09 20:09:34 +0300

    feat(interface): define Orders interface for allOrders object

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,26 +1,35 @@
-export default interface EnvConfig{
-    port:string,
-    secret_key:string,
-    client_origin: string,
-    senderEmail: string,
-    password:string
+export default interface EnvConfig {
+  port: string;
+  secret_key: string;
+  client_origin: string;
+  senderEmail: string;
+  password: string;
 }
 
 export type OrderDetails = [
-    string,     // orderId
-    number,     // createdBy
-    string,     // service
-    string,     // subject
-    string,     // gradeLevel
-    string,     // style
-    string,     // language
-    string,     // sources
-    string,     // newFiles
-    string,     // instructions
-    string,     // topic
-    string,     // pagesOrwords
-    string,     // amount
-    string,     // deadline
-    string,     // time
-    string      // status
+  string, // orderId
+  number, // createdBy
+  string, // service
+  string, // subject
+  string, // gradeLevel
+  string, // style
+  string, // language
+  string, // sources
+  string, // newFiles
+  string, // instructions
+  string, // topic
+  string, // pagesOrwords
+  string, // amount
+  string, // deadline
+  string, // time
+  string // status
 ];
+
+export interface Orders {
+  username: string;
+  totalOrders: number;
+  allActiveOrders: number;
+  allCancelledOrders: number;
+  allCompletedOrders: number;
+  allOrders: [] | {}[];
+}
